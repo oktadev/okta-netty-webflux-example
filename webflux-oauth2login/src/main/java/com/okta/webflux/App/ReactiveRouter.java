a4a671832b4160d04a1caf9b63accc63f202b958
Andrew Hughes <andrewcarterhughes@gmail.com>
2019-09-20 08:51:57 -0700

    added webflux oauth2

---
+++ webflux-oauth2login/src/main/java/com/okta/webflux/App/ReactiveRouter.java
@@ -0,0 +1,22 @@
+package com.okta.webflux.App;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class ReactiveRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(ReactiveHandler handler) {
+
+        return RouterFunctions
+            .route(RequestPredicates
+                .GET("/")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), handler::hello);
+    }
+}
